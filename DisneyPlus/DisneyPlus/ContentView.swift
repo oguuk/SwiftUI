0317f468209b37f5c2044619544b9d8e9a474482
oguuk <ogw135@gmail.com>
2023-05-22 23:11:35 +0900

    feat: Imple ContetView

--- DisneyPlus/DisneyPlus/ContentView.swift
+++ DisneyPlus/DisneyPlus/ContentView.swift
@@ -8,19 +8,76 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let homeView = HomeView()
+    let searchView = SearchView()
+    let downloadView = DownloadView()
+    let profileView = ProfileView()
+    
+    @StateObject var tabViewRouter: TabViewRouter
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        GeometryReader { geo in
+            VStack(spacing: -10) {
+                getTabPage()
+                getTabs(geo: geo)
+                
+            }
+            .edgesIgnoringSafeArea(.all)
         }
-        .padding()
+    }
+    
+    @ViewBuilder //  반환하고 싶은 View 타입이 여러 종류일때
+    private func getTabPage() -> some View {
+        switch tabViewRouter.currentPage {
+        case .home:
+            homeView
+        case .search:
+            searchView
+        case .downloads:
+            downloadView
+        case .profile:
+            profileView    
+        }
+    }
+    
+    private func getTabs(geo: GeometryProxy) -> some View {
+        return HStack {
+            TabBarIcon(
+                tabViewRouter: tabViewRouter,
+                page: .home,
+                geo: geo,
+                systemIconName: "house",
+                tabName: "Home")
+            TabBarIcon(
+                tabViewRouter: tabViewRouter,
+                page: .search,
+                geo: geo,
+                systemIconName: "magnifyingglass",
+                tabName: "Search")
+            TabBarIcon(
+                tabViewRouter: tabViewRouter,
+                page: .downloads,
+                geo: geo,
+                systemIconName: "square.and.arrow.down",
+                tabName: "Downloads")
+            TabBarIcon(
+                tabViewRouter: tabViewRouter,
+                page: .profile,
+                geo: geo,
+                systemIconName: "person.circle",
+                tabName: "Profile")
+        }
+        .frame(width: geo.size.width, height: 60)
+        .padding(.bottom, 20)
+        .background(Color.white.shadow(radius: 2))
+        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(tabViewRouter: TabViewRouter())
+            .preferredColorScheme(.dark)
     }
 }
